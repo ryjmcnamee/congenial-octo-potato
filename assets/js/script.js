3ad1adf28dccc3a959a0cd03d0255398a7d04021
ryjmcnamee <ryjmcnamee@gmail.com>
2022-09-19 21:15:51 -0700

    add search function to navigate to secondary page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-var searchRequestSubmit = document.
+var searchRequestSubmit = document.getElementById("indexForm");
 
 function onSearchClick(event){
-    var searchTerm = 
+    event.preventDefault();
+    var searchTerm = event.getElementById("query");
+    var searchFormat = event.getElementById("format");
+    window.open("../../search-results.html?" + "searchTerm=" + searchTerm + "&searchFormat=" + searchFormat);
 }
 
 searchRequestSubmit.addEventListener("submit", onSearchClick)
